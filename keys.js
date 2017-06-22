07d5695c2aaf2ed175f98aa900172984ed81d200
John Bendfeldt <jfbendfeldt@gmail.com>
2017-06-22 14:02:29 -0500

    inital files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '67pb89B6XDNmlzkjzmEmejQYN',
+  consumer_secret: 'c3RjUjFiB4wmVqMBqoobXqiG3l6TxVKaCsqWafCilOg6fxigBi',
+  access_token_key: '876546383758729216-3fXT0qks9OpCUWrI4MCNRWn7EPF0nl9',
+  access_token_secret: 'KQjy3w9nqU9YGFrCrnFYqZ7eM2H2dbOUlJMofhWFi0g8a',
+}
